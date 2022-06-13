5f8db230cea348ea92b9bc7e9d9a64d6d3005983
CrossNox <ijmermet@gmail.com>
2022-06-13 00:42:37 -0300

    ejemplos de zmq

---
+++ zmq/wuserver.py
@@ -0,0 +1,23 @@
+#
+#   Weather update server
+#   Binds PUB socket to tcp://*:5556
+#   Publishes random weather updates
+#
+
+from random import randrange
+
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.bind("tcp://*:5556")
+
+while True:
+    zipcode = randrange(1, 100000)
+    temperature = randrange(-80, 135)
+    relhumidity = randrange(10, 60)
+
+    if zipcode == 10_001:
+        print(f"{zipcode} {temperature} {relhumidity}")
+
+    socket.send_string(f"{zipcode} {temperature} {relhumidity}")
